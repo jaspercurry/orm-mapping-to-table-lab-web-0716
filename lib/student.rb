d7dbd834b34635015fb51c784c9294ee0db364da
jaspercurry <jaspercurry@gmail.com>
2016-08-05 15:48:36 -0400

    build student class that interacts with database

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,40 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    GRADE TEXT)"
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = "INSERT INTO students (name, grade)
+    VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    sql = "SELECT id FROM students ORDER BY id DESC LIMIT 1"
+    x = DB[:conn].execute(sql)
+    @id=(x[0][0])
+  end
+
+  def self.create(name:, grade:)
+    new_student = Student.new(name, grade)
+    new_student.save
+    new_student
+  end
 end
+
